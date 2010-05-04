9360aad78ba6b9b4f4a8ddec82798ad86f75cb4e
Dave Abrahams <dave@boostpro.com>
2010-05-04 14:42:18 -0400

    Slightly different URL format

--- gen_index.py
+++ gen_index.py
@@ -7,12 +7,13 @@ here = os.path.dirname(
 for d in (x for x in os.listdir(here) if os.path.isdir(x) and not x.startswith('.')):
     shutil.rmtree(d)
 
-index_template = """<html><head><title>Links for boost-python</title></head><body><h1>Links for %(name)s</h1><a href="git+http://git.%(url_tail)s#egg=%(name)s">Git Repository</a><br/> 
+index_template = """<html><head><title>Links for boost-python</title></head><body><h1>Links for %(name)s</h1><a href="git+http://git.%(url_tail)s#egg=%(normal_name)s">Git Repository</a><br/> 
 <a href="http://www.boost.org/doc/libs/1_42_0/libs/%(name_tail)s/doc/index.html" rel="homepage">1.42.0 home_page</a><br/> 
 </body></html>"""
 
 for name,url in repos.iteritems():
     os.mkdir(os.path.join(here,name))
+    normal_name = name.replace('.','-')
     assert url.startswith('git://')
     url_tail = url[len('git://'):]
     name_tail = name[name.find('.')+1:]
